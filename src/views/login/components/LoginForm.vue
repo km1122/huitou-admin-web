09040791a071fcbb555540a8cea9ee3c4019df85
kaiminchen <kaiminchen@tencent.com>
2025-09-04 11:37:46 +0800

    移除ElNotification组件

--- src/views/login/components/LoginForm.vue
+++ src/views/login/components/LoginForm.vue
@@ -33,7 +33,6 @@ import { useRouter } from "vue-router";
 import { HOME_URL } from "@/config";
 // import { getTimeState } from "@/utils";
 import { Login } from "@/api/interface";
-import { ElNotification } from "element-plus";
 import { loginApi } from "@/api/modules/login";
 import { useUserStore } from "@/stores/modules/user";
 import { useTabsStore } from "@/stores/modules/tabs";
@@ -87,7 +86,6 @@ const login = (formEl: FormInstance | undefined) => {
       //   type: "success",
       //   duration: 3000
       // });
-
     } finally {
       loading.value = false;
     }
